864382e20c7ab240721fe0f1a39e9788156b3014
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-09-20 18:46:42 +0530

    test: added fmap with nothing return type

--- index.test.ts
+++ index.test.ts
@@ -2,6 +2,11 @@ import { expect, test } from "bun:test";
 import { fmapMaybe } from ".";
 
 // test the fmaptype
-test("fmap test", () => {
+test("fmap with value", () => {
   expect(fmapMaybe((x) => x + 3, { just: 2 })).toStrictEqual({ just: 5 });
+});
+
+//fmap test for nothing type
+test("fmap with nothing", () => {
+  expect(fmapMaybe((x) => x + 3, null)).toStrictEqual(null);
 });
--- index.ts
+++ index.ts
@@ -9,8 +9,11 @@ type Maybe<A> = Nothing | Just<A>;
 //fmap -> to know how to apply functions to values that are wrapped in a context
 
 export const fmapMaybe = <A, B>(f: (a: A) => B, m: Maybe<A>): Maybe<B> => {
+  //if we have nothing, then return nothing
   if (!m) {
     return null;
   }
+  // if we have a just type, return a just with the function
+  // applies to the contents.
   return { just: f(m.just) };
 };
